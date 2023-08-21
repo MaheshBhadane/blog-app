145fbdf5b0f74148a92fdbaadb6619a1c411327c
Mahesh Bhadane <108877616+MaheshBhadane@users.noreply.github.com>
2023-08-22 00:23:17 +0530

    git commit --amend --no-edit

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-"use client"; // Error components must be Client Components
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
@@ -12,21 +12,13 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
     <div className="flex items-center justify-center">
       <h2>Something went wrong!</h2>
-      <Button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </Button>
+      <Button onClick={() => reset()}>Try again</Button>
     </div>
   );
 }
